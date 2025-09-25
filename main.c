57cf258907339ac4d7f6bbae5aeaae5451b7d9bf
Jeremy  Smith <jeremy.ws.2008@gmail.com>
2025-09-25 12:31:25 -0500

    Added function to format file sizes

--- main.c
+++ main.c
@@ -2,10 +2,47 @@
 A user-friendly CLI utility to print file information
 */
 
+//Included for function fstat + struct stat
 #include <sys/stat.h>
+//Included for getting file descriptor
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+
+//This string MUST BE FREED
+char* size_formatted(off_t st_size){
+    char* buf;
+    double fsize;
+    if (st_size < 1e3){
+        asprintf(&buf, "%lld bytes", st_size);
+    }
+    else if (st_size < 1e6){
+        fsize = (double) st_size/1e3;
+        asprintf(&buf, "%.3f kilobytes", fsize);
+    }
+    else if (st_size < 1e9){
+        fsize = (double) st_size/1e6;
+        asprintf(&buf, "%.3f megabytes", fsize);
+    }
+    else if (st_size < 1e12){
+        fsize = (double) st_size/1e9;
+        asprintf(&buf, "%.3f gigabytes", fsize);
+    }
+    else if (st_size < 1e15){
+        fsize = (double) st_size/1e12;
+        asprintf(&buf, "%.3f terabytes", fsize);
+    }
+    else{
+        fsize = (double) st_size/1e15;
+        asprintf(&buf, "%.3f petabytes", fsize);
+    }
+
+    return buf;
+    
+
+}
+
 int main(int nargs, char** args){
     
     if (nargs != 2){
@@ -22,27 +59,27 @@ int main(int nargs, char** args){
         return -1;
     }
 
-    char* restrict canonizalized = realpath(args[1], NULL);
+    char* __restrict__ canonizalized = realpath(args[1], NULL);
 
     if (!canonizalized){
         printf("Error: could not canonicalize path");
         return -1;
     }
-
-
-
+    
+    char* restrict filesize_formatted = size_formatted(file_stat.st_size);
 
     printf("Info for: %s\n", args[1]);
 
     //Print full path
-    printf("Full path: %s", canonizalized); 
+    printf("Full path: %s\n", canonizalized); 
     //Print size
+    printf("Size: %s\n", filesize_formatted);
     //Print date-time created
     //Print date-time of last modification
     //Print permissions
 
-
     free(canonizalized);
+    free(filesize_formatted);
 
     return 0;
     
